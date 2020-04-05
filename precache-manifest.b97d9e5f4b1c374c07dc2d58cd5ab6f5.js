9b0b6564a9a6907326ee0e5d1d6d3f2b8466816a
tenezill <tenezill@gmail.com>
2020-04-05 14:39:47 +0200

    Updates

---
+++ precache-manifest.b97d9e5f4b1c374c07dc2d58cd5ab6f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb80f76e563aea8a120c0418bc92cb0",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "8c0a69aec153fb048a5f",
+    "url": "/Monsters-Rolodex/static/css/main.70515a0b.chunk.css"
+  },
+  {
+    "revision": "bd500d412ead297d0f07",
+    "url": "/Monsters-Rolodex/static/js/2.0c09e5d0.chunk.js"
+  },
+  {
+    "revision": "8c0a69aec153fb048a5f",
+    "url": "/Monsters-Rolodex/static/js/main.d8b55916.chunk.js"
+  },
+  {
+    "revision": "589b79cfa71ba52b30b0",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.62afa2e7.js"
+  }
+]);
